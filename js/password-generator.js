122d46b41d2b328efa39171a9101278675616910
lifeconsciousness <lifeconsciousness321@gmail.com>
2023-01-01 05:43:42 +0100

    generates random passwords with all conditions

--- js/password-generator.js
+++ js/password-generator.js
@@ -6,7 +6,7 @@ const sliderValueDisplay = document.querySelector(`.password-length`)
 const lowcaseCheckbox = document.querySelector(`#lowercase-checkbox`)
 const uppercaseCheckbox = document.querySelector(`#uppercase-checkbox`)
 const numbersCheckbox = document.querySelector(`#numbers-checkbox`)
-const symbolsCheckbox = document.querySelector(`symbols-checkbox`)
+const symbolsCheckbox = document.querySelector(`#symbols-checkbox`)
 const checkboxes = document.querySelectorAll(`.checkbox`)
 
 const lowercaseArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","r","s","t","u","v","w","x","y","z"]
@@ -16,32 +16,80 @@ const symbolsArray = ["!","#","'","$","%","^",":",";","&","*","(",")"]
 
 const generateButton = document.querySelector(`.generate-btn`)
 let result = ``
+let includesArray = []
 
+//updates password length display
 rangeSlider.oninput = function(){
     sliderValueDisplay.innerHTML = rangeSlider.value
 }
 
-generateButton.onclick = function(){
+generateButton.onclick = function generatePassword(){
+    //reset the variables
     result = ``
+    includesArray = []
+
+    //check for all empty checkboxes and get representative numbers of checked ones 
     if(checkEmptyCheckboxes()){
         return
     }
+    getIncludesNumbers()
 
+    //get string of random calls to different arrays of characters
+    let differentIncludes = generateDifferentIncludes(includesArray)
 
+    //fill the result sting with random characters
     for(let i = 0; i < rangeSlider.value; i++){
-        if(lowcaseCheckbox.checked){
+        if(differentIncludes[i] == 1){
             let randomIndex = Math.floor(Math.random() * lowercaseArray.length)
             result += lowercaseArray[randomIndex] 
         }
+        if(differentIncludes[i] == 2){
+            let randomIndex = Math.floor(Math.random() * uppercaseArray.length)
+            result += uppercaseArray[randomIndex] 
+        }
+        if(differentIncludes[i] == 3){
+            let randomIndex = Math.floor(Math.random() * numbersArray.length)
+            result += numbersArray[randomIndex] 
+        }
+        if(differentIncludes[i] == 4){
+            let randomIndex = Math.floor(Math.random() * symbolsArray.length)
+            result += symbolsArray[randomIndex] 
+        }
     }
 
-    
     passwordDisplay.innerHTML = result
 }
 
 
+//fills array with numbers that represent different symbols,letters inclusion
+function getIncludesNumbers(){
+    if(lowcaseCheckbox.checked){
+        includesArray.push(1)
+    }
+    if(uppercaseCheckbox.checked){
+        includesArray.push(2)
+    }
+    if(numbersCheckbox.checked){
+        includesArray.push(3)
+    }
+    if(symbolsCheckbox.checked){
+        includesArray.push(4)
+    }
+}
 
+//takes numbers that represent checked checkboxes (lowercase,uppercase,numbers, etc)
+function generateDifferentIncludes(includesNumbers){
+    let includesString = ``
 
+    for(let i = 0; i < rangeSlider.value; i++){
+        let numbersRandomIndex = Math.floor(Math.random() * includesNumbers.length)
+        includesString += includesNumbers[numbersRandomIndex]
+    }
+
+    return includesString
+}
+
+//checks whether all checkboxes are empty and retures true/false value
 function checkEmptyCheckboxes(){
     let counter = 0;
     checkboxes.forEach((checkbox)=>{
@@ -53,5 +101,4 @@ function checkEmptyCheckboxes(){
         passwordDisplay.innerHTML = "empty"
         return true
     }
-    return false
 }
